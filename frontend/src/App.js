7c937c6f921871addb5a7bd66e7312cde51fc18d
Grace Hanson <hansongrace2004@gmail.com>
2025-10-03 20:31:00 -0400

    reset bg color to lighter green

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 
 export default function App() {
   return (
-    <div style={{ backgroundColor: "green", height: "100vh", margin: 0 }}>
+    <div style={{ backgroundColor: "#95F2AB", height: "100vh", margin: 0 }}>
+
     </div>
   );
 }
