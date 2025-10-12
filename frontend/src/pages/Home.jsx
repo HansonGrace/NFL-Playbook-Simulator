f640362ed652b7d584c71331545d68251a21bdf6
Grace Hanson <hansongrace2004@gmail.com>
2025-10-12 14:22:03 -0400

    add backend with spring initializer

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,12 +5,10 @@ import "../styles/Home.css";
 export default function Home() {
   return (
     <main className="home-root">
-      <div className="home-content">
-        <img src={logo} className="home-logo" alt="PlaySimulator Logo" />
-        <div className="home-text">
-          <p>Live NFL Playbook Simulations Using Real Data</p>
-        </div>
+      <div className="home-center">
+        <img src={logo} className="home-logo" alt="Title logo" />
       </div>
     </main>
   );
 }
+
